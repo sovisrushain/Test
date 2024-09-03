8918cfab57020938dcffe25e7650dce019a79d55
sovisrushain <sovisrushain@gmail.com>
2024-09-03 19:50:25 +0530

    initialize app

---
+++ todo/src/test/java/com/cisco/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cisco.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
