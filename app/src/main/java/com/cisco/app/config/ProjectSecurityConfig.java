7cf96e68724abf28e9c3710a20d0e743b90c61eb
sovisrushain <sovisrushain@gmail.com>
2024-09-20 12:47:41 +0530

    disable formLogin and httpBasic authentication

--- app/src/main/java/com/cisco/app/config/ProjectSecurityConfig.java
+++ app/src/main/java/com/cisco/app/config/ProjectSecurityConfig.java
@@ -3,6 +3,7 @@ package com.cisco.app.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -15,8 +16,8 @@ public class ProjectSecurityConfig {
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/account", "/balance", "/loans", "/card").authenticated()
                 .requestMatchers("/notices", "/contact", "/error").permitAll());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
